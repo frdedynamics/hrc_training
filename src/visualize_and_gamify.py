e355fe0728a5747c6b4cec4484850979889420cc
Gizem <gizem.ates@hvl.no>
2022-05-23 17:16:52 +0200

    backup -- fix arm orientations

--- src/visualize_and_gamify.py
+++ src/visualize_and_gamify.py
@@ -2,6 +2,7 @@
 
 import rospy
 import tf2_ros
+import numpy as np
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 from std_msgs.msg import String
@@ -9,8 +10,14 @@ from geometry_msgs.msg import Pose, Quaternion
 import tf.transformations
 
 from Classes.MarkerBasics import MarkerBasics
+import rospkg, sys
+imu_pkg = rospkg.RosPack()
+imu_pkg_path = imu_pkg.get_path('imu_human_pkg')+"/src/Classes"
+# print(imu_pkg_path)
+sys.path.insert(0, imu_pkg_path)
+import Kinematics_with_Quaternions as kinematic
 
-ref = 'human/shoulder_center'
+ref = 'base_link'
 
 def main():
     rospy.init_node('rviz_markers', anonymous=True)
@@ -22,8 +29,8 @@ def main():
     left_arm_marker = MarkerBasics(topic_id="human/left_shoulder")
     right_arm_marker = MarkerBasics(topic_id="human/right_shoulder")
 
-    right_bias = [0.0, 0.7, 0.0, 0.7]
-    left_bias = [0.0, 0.7, 0.0, 0.7]
+    # right_bias = Quaternion([0, 0, 0.7, 0.7])
+    # left_bias = Quaternion([0, 0, 0.7, 0.7])
     
     while not rospy.is_shutdown():
         try:
@@ -37,8 +44,11 @@ def main():
         left_arm_marker.marker_object.pose.position = left_shoulder_trans.transform.translation
         left_arm_marker.marker_object.pose.orientation = left_shoulder_trans.transform.rotation
         # left_arm_marker.marker_object.pose.orientation = tf.transformations.quaternion_multiply( right_bias,[left_shoulder_trans.transform.rotation.x, left_shoulder_trans.transform.rotation.y, left_shoulder_trans.transform.rotation.z, left_shoulder_trans.transform.rotation.w])
-        print(left_shoulder_trans.transform.rotation.x)
-        print(left_arm_marker.marker_object.pose.orientation.x)
+        # print(left_shoulder_trans.transform.rotation.x)
+        # print(left_arm_marker.marker_object.pose.orientation.x)
+        # print(type(left_shoulder_trans.transform.rotation))
+        # left_arm_marker.marker_object.pose.orientation = kinematic.kinematic.q_multiply(left_shoulder_trans.transform.rotation, left_bias)
+        # print(left_arm_marker.marker_object.pose.orientation)
         right_arm_marker.marker_object.pose.position = right_shoulder_trans.transform.translation
         right_arm_marker.marker_object.pose.orientation = right_shoulder_trans.transform.rotation
 
