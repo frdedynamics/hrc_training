a9cae17d5f809e83e7a58cf1f49bd05479c150cd
Gizem <gizem.ates@hvl.no>
2022-07-09 23:32:43 +0200

    visualize markers for colift dir is fixed

--- src/visualize_and_gamify.py
+++ src/visualize_and_gamify.py
@@ -8,9 +8,21 @@ from Classes.MarkerBasics import MarkerBasics
 
 ref = 'human/base'
 
+elbow_height_th = 0.0
+
+elbow_left_height = 0.0
+elbow_right_height = 0.0
 force_mode = String()
 hrc_state = String()
 score_val = Int16()
+
+def cb_elbow_left( msg):
+    global elbow_left_height
+    elbow_left_height = msg.position.y
+    
+def cb_elbow_right( msg):
+    global elbow_right_height
+    elbow_right_height = -msg.position.y
 
 def cb_force_mode(msg):
     global force_mode
@@ -27,16 +39,18 @@ def cb_score_val(msg):
 
 
 def main():
-    global force_mode
+    global elbow_right_height, elbow_left_height, elbow_height_th
     rospy.init_node('rviz_markers', anonymous=True)
     rate = rospy.Rate(100)
 
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
-    sub_colift_dir = rospy.Subscriber('/colift_dir', String, cb_force_mode)
     sub_hrc_state = rospy.Subscriber('/hrc_state', String, cb_hrc_state)
+    sub_colift_dir = rospy.Subscriber('/colift_dir', String, cb_force_mode)
     sub_score_val = rospy.Subscriber('/score_val', Int16, cb_score_val)
+    sub_elbow_left = rospy.Subscriber('/elbow_left', Pose, cb_elbow_left)
+    sub_elbow_right= rospy.Subscriber('/elbow_right', Pose, cb_elbow_right)
 
     left_arm_marker = MarkerBasics(topic_id="human/left_shoulder_", type="arm")
     right_arm_marker = MarkerBasics(topic_id="human/right_shoulder_", type="arm")
@@ -48,7 +62,17 @@ def main():
     hrc_state_str_marker.change_scale(0.8, 0.8, 0.8)
     hrc_state_str_marker.change_colour(1.0, 1.0, 1.0)
 
-    force_mode.data = "s"
+    try:
+        while not rospy.has_param("/elbow_height_th"):
+            print("no elbow parameter set")
+            force_mode.data = "s"
+            rospy.sleep(1)
+
+    except rospy.ROSInterruptException:
+        pass
+
+    elbow_height_th = rospy.get_param("/elbow_height_th")
+    print("ELBOW PARAM SET")
 
     while not rospy.is_shutdown():
         try:
@@ -65,32 +89,45 @@ def main():
         right_arm_marker.marker_object.pose.position = right_shoulder_trans.transform.translation
         right_arm_marker.marker_object.pose.orientation = right_shoulder_trans.transform.rotation
 
-        if force_mode.data == "u":
+        print("elbow_left_height: ", elbow_left_height)
+        print("elbow_right_height: ", elbow_right_height)
+        print("force_mode: ", force_mode.data)
+        print("elbow_height_th: ", elbow_height_th)
+        
+
+        if(elbow_left_height < elbow_height_th) and (elbow_right_height < elbow_height_th):
+            if(force_mode.data == "s"):
+                left_arm_marker.set_visible()
+                left_arm_marker.change_colour(R=0, G=0, B=255)
+                right_arm_marker.set_visible()
+                right_arm_marker.change_colour(R=0, G=0, B=255)
+                colift_dir_str_marker.update_str_marker("DOWN", R=0, G=0, B=255)
+                print("RIGH")
+            else: # force_mode.data = "d"
+                left_arm_marker.set_visible(transparancy=0.2)
+                right_arm_marker.set_visible(transparancy=0.2)            
+                colift_dir_str_marker.update_str_marker("STOP", R=0, G=0, B=0)
+                print("DOWN")
+
+        elif((elbow_left_height > elbow_height_th) and (elbow_right_height > elbow_height_th)):
             left_arm_marker.set_visible()
             left_arm_marker.change_colour(R=0, G=255, B=0)
             right_arm_marker.set_visible()
             right_arm_marker.change_colour(R=0, G=255, B=0)
             colift_dir_str_marker.update_str_marker("UP", R=0, G=255, B=0)
-        elif force_mode.data == "d":
-            left_arm_marker.set_visible()
-            left_arm_marker.change_colour(R=0, G=0, B=255)
-            right_arm_marker.set_visible()
-            right_arm_marker.change_colour(R=0, G=0, B=255)
-            colift_dir_str_marker.update_str_marker("DOWN", R=0, G=0, B=255)
-        elif force_mode.data == "l":
+            print("UP")
+        elif((elbow_left_height > elbow_height_th) and (elbow_right_height < elbow_height_th)):
             left_arm_marker.set_visible()
             left_arm_marker.change_colour(R=0, G=255, B=0)
             right_arm_marker.set_invisible()
             colift_dir_str_marker.update_str_marker("LEFT", R=0, G=255, B=0)
-        elif force_mode.data == "r":
+            print("LEFT")
+        elif((elbow_right_height > elbow_height_th) and (elbow_left_height < elbow_height_th)):
             left_arm_marker.set_invisible()
             right_arm_marker.set_visible()
             right_arm_marker.change_colour(R=0, G=255, B=0)
             colift_dir_str_marker.update_str_marker("RIGHT", R=0, G=255, B=0)
-        elif force_mode.data == "s":
-            left_arm_marker.set_visible(transparancy=0.2)
-            right_arm_marker.set_visible(transparancy=0.2)            
-            colift_dir_str_marker.update_str_marker(" ", R=0, G=0, B=0)
+            print("RIGHT")
         else:
             print("No force mode")
             left_arm_marker.set_invisible()
