4f35b3e51d1f4afe98adf1dab6a1e11e55a1e53b
Gizem <gizem.ates@hvl.no>
2022-07-10 02:36:28 +0200

    tcp_force_marker is created and visualized correctly

--- src/visualize_and_gamify.py
+++ src/visualize_and_gamify.py
@@ -2,7 +2,7 @@
 
 import rospy, sys
 import tf2_ros
-from std_msgs.msg import String, Int16
+from std_msgs.msg import String, Int16, Float32MultiArray
 from geometry_msgs.msg import Pose, Quaternion
 from Classes.MarkerBasics import MarkerBasics
 
@@ -12,6 +12,8 @@ elbow_height_th = 0.0
 
 elbow_left_height = 0.0
 elbow_right_height = 0.0
+tcp_force = Float32MultiArray()
+tcp_force.data = 6*[0.0]
 force_mode = String()
 hrc_state = String()
 score_val = Int16()
@@ -28,6 +30,10 @@ def cb_force_mode(msg):
     global force_mode
     force_mode = msg
 
+def cb_tcp_force(msg):
+    global tcp_force
+    tcp_force = msg
+
 def cb_hrc_state(msg):
     global hrc_state
     hrc_state = msg
@@ -39,57 +45,71 @@ def cb_score_val(msg):
 
 
 def main():
-    global elbow_right_height, elbow_left_height, elbow_height_th
+    global elbow_right_height, elbow_left_height, elbow_height_th, tcp_force
     rospy.init_node('rviz_markers', anonymous=True)
     rate = rospy.Rate(100)
 
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
-    sub_hrc_state = rospy.Subscriber('/hrc_state', String, cb_hrc_state)
-    sub_colift_dir = rospy.Subscriber('/colift_dir', String, cb_force_mode)
-    sub_score_val = rospy.Subscriber('/score_val', Int16, cb_score_val)
-    sub_elbow_left = rospy.Subscriber('/elbow_left', Pose, cb_elbow_left)
-    sub_elbow_right= rospy.Subscriber('/elbow_right', Pose, cb_elbow_right)
-
     left_arm_marker = MarkerBasics(topic_id="human/left_shoulder_", type="arm")
     right_arm_marker = MarkerBasics(topic_id="human/right_shoulder_", type="arm")
     score_marker = MarkerBasics(topic_id="score_", type="score")
     colift_dir_str_marker = MarkerBasics(topic_id="colift_dir_", type="regular_str")
     hrc_state_str_marker = MarkerBasics(topic_id="hrc_state_", type="regular_str")
+    tcp_force_marker = MarkerBasics(topic_id="tcp_force_", type="regular_str")
+
+    sub_hrc_state = rospy.Subscriber('/hrc_state', String, cb_hrc_state)
+    sub_colift_dir = rospy.Subscriber('/colift_dir', String, cb_force_mode)
+    sub_score_val = rospy.Subscriber('/score_val', Int16, cb_score_val)
+    sub_elbow_left = rospy.Subscriber('/elbow_left', Pose, cb_elbow_left)
+    sub_elbow_right= rospy.Subscriber('/elbow_right', Pose, cb_elbow_right)
+    sub_tcp_force= rospy.Subscriber('/tcp_force', Float32MultiArray, cb_tcp_force)
 
     hrc_state_str_marker.change_position(0, 0, 1.6)
     hrc_state_str_marker.change_scale(0.8, 0.8, 0.8)
     hrc_state_str_marker.change_colour(1.0, 1.0, 1.0)
 
-    while not rospy.has_param("/elbow_height_th"):
-            print("no elbow parameter set")
-            rospy.sleep(1)
-            if rospy.is_shutdown():
-                sys.exit()
-    elbow_height_th = rospy.get_param("/elbow_height_th")
-    print("ELBOW PARAM SET")
+    tcp_force_marker.change_position(0, 0, -0.2)
+    tcp_force_marker.change_scale(5.0, 5.0, 5.0)
+    tcp_force_marker.change_colour(1.0, 1.0, 1.0)
+    tcp_force_marker.update_str_marker('0')
 
-    while not rospy.has_param("/robot_move_started"):
-        print("no /robot_move_started parameter set")
-        rospy.sleep(1)
-        if rospy.is_shutdown():
-                sys.exit()
+    # while not rospy.has_param("/elbow_height_th"):
+    #         print("no elbow parameter set")
+    #         rospy.sleep(1)
+    #         if rospy.is_shutdown():
+    #             sys.exit()
+    # elbow_height_th = rospy.get_param("/elbow_height_th")
+    # print("ELBOW PARAM SET")
 
-    print("/robot_move_started SET")
+    # while not rospy.has_param("/robot_move_started"):
+    #     print("no /robot_move_started parameter set")
+    #     rospy.sleep(1)
+    #     if rospy.is_shutdown():
+    #             sys.exit()
 
-    while not rospy.has_param("/colift_set"):
-        print("no /colift_set parameter set")
-        force_mode.data = "u"
-        rospy.sleep(1)
-        if rospy.is_shutdown():
-                sys.exit()
+    # print("/robot_move_started SET")
 
-    print("/colift_set SET")
+    # while not rospy.has_param("/colift_set"):
+    #     print("no /colift_set parameter set")
+    #     force_mode.data = "u"
+    #     rospy.sleep(1)
+    #     if rospy.is_shutdown():
+    #             sys.exit()
+
+    # print("/colift_set SET")
 
     
 
     while not rospy.is_shutdown():
+        
+        tcp_force_marker.set_visible(tcp_force.data[1]/30.0)
+        if abs(tcp_force.data[1]) > 20:
+            tcp_force_marker.change_colour(0.0, 1.0, 0.0)
+        else:
+            tcp_force_marker.change_colour(1.0, 1.0, 1.0)
+
         try:
             left_shoulder_trans = tfBuffer.lookup_transform(ref, 'human/left_shoulder', rospy.Time())
             right_shoulder_trans = tfBuffer.lookup_transform(ref, 'human/right_shoulder', rospy.Time())
@@ -151,6 +171,7 @@ def main():
         score_marker.update_score_marker(score_val)
         hrc_state_str_marker.update_str_marker(hrc_state.data)
         hrc_state_str_marker.marker_objectlisher.publish(hrc_state_str_marker.marker_object)
+        tcp_force_marker.marker_objectlisher.publish(tcp_force_marker.marker_object)
         left_arm_marker.marker_objectlisher.publish(left_arm_marker.marker_object)
         right_arm_marker.marker_objectlisher.publish(right_arm_marker.marker_object)
         score_marker.marker_objectlisher.publish(score_marker.marker_object)
